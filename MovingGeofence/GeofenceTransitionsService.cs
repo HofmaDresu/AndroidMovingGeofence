5114a043dbb739f8deea5747e5559cd823f13bb9
Matt Hoffman <hofmadresu@gmail.com>
2018-05-07 12:40:23 -0400

    Create moving geofence

--- MovingGeofence/GeofenceTransitionsService.cs
+++ MovingGeofence/GeofenceTransitionsService.cs
@@ -1,10 +1,72 @@
 ﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Gms.Location;
+using Android.Graphics;
+using Android.Support.V4.App;
+
 namespace MovingGeofence
 {
-    public class GeofenceTransitionsService
-    {
-        public GeofenceTransitionsService()
-        {
-        }
-    }
+	[Service]
+	public class GeofenceTransitionsService : IntentService
+	{
+		protected override void OnHandleIntent(Intent intent)
+		{
+			var ge = GeofencingEvent.FromIntent(intent);
+
+			var transition = ge.GeofenceTransition;
+
+			if (transition == Geofence.GeofenceTransitionExit)
+			{
+				var notificationIntent = new Intent(ApplicationContext, typeof(MainActivity));
+
+                var stackBuilder = Android.Support.V4.App.TaskStackBuilder.Create(this);
+                stackBuilder.AddParentStack(Java.Lang.Class.FromType(typeof(MainActivity)));
+                stackBuilder.AddNextIntent(notificationIntent);
+
+                var notificationPendingIntent = stackBuilder.GetPendingIntent(0, (int)PendingIntentFlags.UpdateCurrent);
+
+                var builder = new NotificationCompat.Builder(this);
+                builder.SetSmallIcon(Resource.Mipmap.Icon)
+                    .SetColor(Color.Red)
+                    .SetContentTitle("Location changed")
+                    .SetContentText("Test")
+                    .SetContentIntent(notificationPendingIntent);
+
+                builder.SetAutoCancel(true);
+
+                var mNotificationManager = (NotificationManager)GetSystemService(Context.NotificationService);
+				mNotificationManager?.Notify(0, builder.Build());
+
+				SetGeofence();
+			}
+		}
+
+		private async void SetGeofence() 
+		{
+            var locationClient = LocationServices.GetFusedLocationProviderClient(this);
+
+
+            var geofencingClient = LocationServices.GetGeofencingClient(this);
+
+            var lastLocation = await locationClient.GetLastLocationAsync();
+
+            var geoFence = new GeofenceBuilder()
+                .SetRequestId("fence")
+                .SetCircularRegion(lastLocation.Latitude, lastLocation.Longitude, 100)
+                .SetTransitionTypes(Geofence.GeofenceTransitionExit | Geofence.GeofenceTransitionEnter)
+                .SetExpirationDuration(Geofence.NeverExpire)
+                .Build();
+
+            var geofenceRequest = new GeofencingRequest.Builder()
+                                                       .SetInitialTrigger(GeofencingRequest.InitialTriggerEnter)
+                                                       .AddGeofence(geoFence)
+                                                       .Build();
+
+			var geoIntent = new Intent(ApplicationContext, typeof(GeofenceTransitionsService));
+            var pendingGeoIntent = PendingIntent.GetService(this, 0, geoIntent, PendingIntentFlags.UpdateCurrent);
+
+			geofencingClient.AddGeofences(geofenceRequest, pendingGeoIntent);
+		}
+	}
 }
--- MovingGeofence/MainActivity.cs
+++ MovingGeofence/MainActivity.cs
@@ -1,15 +1,23 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Gms.Location;
+using Android.Content;
+using Android.Gms.Tasks;
 
 namespace MovingGeofence
 {
     [Activity(Label = "MovingGeofence", MainLauncher = true, Icon = "@mipmap/icon")]
-    public class MainActivity : Activity
+    public class MainActivity : Activity, IOnCompleteListener
     {
         int count = 1;
 
-        protected override void OnCreate(Bundle savedInstanceState)
+		public void OnComplete(Task task)
+		{
+			var foo = 1;
+		}
+
+		protected override async void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
@@ -21,6 +29,30 @@ namespace MovingGeofence
             Button button = FindViewById<Button>(Resource.Id.myButton);
 
             button.Click += delegate { button.Text = $"{count++} clicks!"; };
+
+			var locationClient = LocationServices.GetFusedLocationProviderClient(this);
+
+
+			var geofencingClient = LocationServices.GetGeofencingClient(this);
+
+			var lastLocation = await locationClient.GetLastLocationAsync();
+
+			var geoFence = new GeofenceBuilder()
+				.SetRequestId("fence")
+				.SetCircularRegion(lastLocation.Latitude, lastLocation.Longitude, 100)
+				.SetTransitionTypes(Geofence.GeofenceTransitionExit | Geofence.GeofenceTransitionEnter)
+				.SetExpirationDuration(Geofence.NeverExpire)
+				.Build();
+
+			var geofenceRequest = new GeofencingRequest.Builder()
+													   .SetInitialTrigger(GeofencingRequest.InitialTriggerEnter)
+													   .AddGeofence(geoFence)
+													   .Build();
+
+			var geoIntent = new Intent(this, typeof(GeofenceTransitionsService));
+			var pendingGeoIntent = PendingIntent.GetService(this, 0, geoIntent, PendingIntentFlags.UpdateCurrent);
+
+			geofencingClient.AddGeofences(geofenceRequest, pendingGeoIntent).AddOnCompleteListener(this);
         }
     }
 }
