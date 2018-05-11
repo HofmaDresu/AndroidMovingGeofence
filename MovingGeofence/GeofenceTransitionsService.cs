40ead8729f1fa8cab00f87a076117c9f3c4e6213
Matt Hoffman <hofmadresu@gmail.com>
2018-05-11 10:21:07 -0400

    Cleanup + update notification message for each change

--- MovingGeofence/GeofenceTransitionsService.cs
+++ MovingGeofence/GeofenceTransitionsService.cs
@@ -10,6 +10,8 @@ namespace MovingGeofence
 	[Service]
 	public class GeofenceTransitionsService : IntentService
 	{
+        public static int foo = 0;
+
 		protected override void OnHandleIntent(Intent intent)
 		{
 			var ge = GeofencingEvent.FromIntent(intent);
@@ -18,20 +20,12 @@ namespace MovingGeofence
 
 			if (transition == Geofence.GeofenceTransitionExit)
 			{
-				var notificationIntent = new Intent(ApplicationContext, typeof(MainActivity));
-
-                var stackBuilder = Android.Support.V4.App.TaskStackBuilder.Create(this);
-                stackBuilder.AddParentStack(Java.Lang.Class.FromType(typeof(MainActivity)));
-                stackBuilder.AddNextIntent(notificationIntent);
-
-                var notificationPendingIntent = stackBuilder.GetPendingIntent(0, (int)PendingIntentFlags.UpdateCurrent);
-
                 var builder = new NotificationCompat.Builder(this);
                 builder.SetSmallIcon(Resource.Mipmap.Icon)
                     .SetColor(Color.Red)
                     .SetContentTitle("Location changed")
-                    .SetContentText("Test")
-                    .SetContentIntent(notificationPendingIntent);
+                    .SetContentText($"Test {foo++}")
+                    .SetPriority(NotificationCompat.PriorityMax);
 
                 builder.SetAutoCancel(true);
 
@@ -59,7 +53,6 @@ namespace MovingGeofence
                 .Build();
 
             var geofenceRequest = new GeofencingRequest.Builder()
-                                                       .SetInitialTrigger(GeofencingRequest.InitialTriggerEnter)
                                                        .AddGeofence(geoFence)
                                                        .Build();
 
